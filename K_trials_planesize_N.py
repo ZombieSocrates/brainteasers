ab173be7bf285edca7cf34bcaccc8e24950784dd
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-02-27 21:57:06 -0600

    Make final tweaks to labels and layout of K_trials_planesize_N

--- K_trials_planesize_N.py
+++ K_trials_planesize_N.py
@@ -12,24 +12,38 @@ def plot_cumulative_plane_processes(K, N, pltgrid_x = 1, pltgrid_y = 1):
 	entire process listed above x * y times.  Defaults to a single
 	one-by-one plot
 	'''
-
+	# Define shared parts of all visuals: the entire figure, 
+	# an outer subplot, and the X axis.
+	fig = plt.figure()
+	outer = fig.add_subplot(1,1,1)
 	X = np.arange(1, K + 1)
 
+	# Plot out the subplots for all batches of trials.
 	for b in range(pltgrid_x * pltgrid_y):
-		ax = plt.subplot(pltgrid_x, pltgrid_y, b + 1)
+		ax = fig.add_subplot(pltgrid_x, pltgrid_y, b + 1)
 		Y = np.array([pp.plane_process(N) for i in X])
 		ax.plot(X, np.cumsum(Y) * 1.0/X, 'bo' if K <= 10 else 'b-')
 		ax.set_xlim(min(X), max(X))
-		ax.set_xlabel('Number of Trials')
 		ax.set_ylim(0,1)
-		ax.set_ylabel('Success Rate')
 		cum_prob = sum(Y) * 1.0/len(Y)
-		ax.set_title("Success Rate for %s Trials: %.2f" % (K, cum_prob))
-    
-	#plt.gca().set_figsize_inches((10,10))
-	plt.suptitle("Crazy Plane with %s Seats: %s batches of %s trials"\
+		ax.set_title("Success Rate: %.2f" % cum_prob)
+
+	# Outer subplot was only there to set shared axis labels.
+	# This just clears everything out and sets the text.
+	outer.spines['top'].set_color('none')
+	outer.spines['bottom'].set_color('none')
+	outer.spines['right'].set_color('none')
+	outer.spines['left'].set_color('none')
+	outer.tick_params(labelcolor = 'w', top = 'off', bottom = 'off',\
+					  left = 'off', right = 'off')
+	outer.set_xlabel('Number of Trials')
+	outer.set_ylabel('Success Rate')
+
+	# Set overall title and provide more space for it.
+	plt.suptitle("Crazy Plane with %s Seats: %s Batches of %s Trials"\
                   % (N, pltgrid_x * pltgrid_y, K))
 	plt.tight_layout()
+	plt.subplots_adjust(top = 0.85)
 	plt.show()
 
 
@@ -40,13 +54,11 @@ if __name__ == '__main__':
 	print("Running four batches of trials with above specifications")
 	plot_cumulative_plane_processes(K, N, pltgrid_x = 2, pltgrid_y = 2)
 
-
-
 '''
 TO DO:
-
-* Add informative labels for the figure returned by `plot_cumulative_plane_process`
 * Change the function so that it saves the file to a .png, perhaps clearing out any 
   extant png files as well.
+* Maybe size the number of plots dynamically, so that it gets as close to square
+  as possible?
 '''
 	
